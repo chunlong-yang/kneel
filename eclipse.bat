f454982d5581b8efb1881c8306c86962337992d8
chunlong, yang <35907456+chunlong-yang@users.noreply.github.com>
2018-02-01 03:10:51 -0600

    Add files via upload

---
+++ eclipse.bat
@@ -0,0 +1,3 @@
+mvn eclipse:eclipse -DdownloadSources=true -Dwtpversion=2.0 
+-- eclipse:clean
+-- mvn install:install-file -DgroupId=com.oracle -DartifactId=ojdbc6 -Dversion=11.2.0.3 -Dpackaging=jar -Dfile=ojdbc6.jar -DgeneratePom=true
